0e9bcf3d48269b6a8c10c9ffd13f44cf5b9a6550
Owen Tran <owen_327@hotmail.com>
2020-09-19 23:52:04 +0100

    create grid and sign grid numbers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-
 function init() {
     console.log("hello world");
+    
+    for (i=1; i<=100; i++){
+        console.log("<div class=\"cell " +i+ "></div>")
+    }
   }
   
 window.addEventListener('DOMContentLoaded', init);
